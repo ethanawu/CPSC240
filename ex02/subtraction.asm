1bfa048ae665bec621e270328b2bd8317f6e9336
ethanawu <ethanwu9@gmail.com>
2024-09-23 18:32:55 -0700

    updated ex02

--- ex02/subtraction.asm
+++ ex02/subtraction.asm
@@ -1,22 +1,21 @@
 section .data
-    SYS_exit     equ 60         ; System call number for exit
-    EXIT_SUCCESS equ 0          ; Exit status
-    num1   dw 0x1234            ; Declare 16-bit signed variable (0x1234)
-    num2   dw 0xFEDC            ; Declare 16-bit signed variable (0xFEDC)
-    dif    dd 0                 ; Declare 32-bit variable (to store the difference)
+    SYS_exit     equ 60         
+    EXIT_SUCCESS equ 0        
+    num1   dw 0x1234            
+    num2   dw 0xFEDC           
+    dif    dd 0                 
     
 section .text
     global _start
 
 _start:
-	mov ax, word[num1]             
-	sub ax, word[num2]   
-	adc dx, 0       
+	mov	eax, [num1]       
+	sub	eax, [num2]   
+	sbb	edx, 0       
 	
-	mov	[dif], ax
-  	mov	[dif+2], dx
+	mov	dword[dif], eax
+  	mov	dword[dif+2], edx
   	
-    	mov rax, SYS_exit           ; Exit system call number
-    	mov rdi, EXIT_SUCCESS       ; Exit status
-    	syscall                     ; Call kernel
-
+    	mov	rax, SYS_exit          
+    	mov	rdi, EXIT_SUCCESS       
+    	syscall 	               
